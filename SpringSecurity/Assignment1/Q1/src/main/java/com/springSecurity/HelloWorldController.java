33c870f0ffb11cd43f167816e1b3eb443d29f2d2
Akhil S Nair <nairakhil8@gmail.com>
2021-03-31 17:36:18 +0530

    SpringSecurity Assignments Complete

---
+++ SpringSecurity/Assignment1/Q1/src/main/java/com/springSecurity/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.springSecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String HelloWorld(){
+        return "Hello World";
+    }
+
+}
